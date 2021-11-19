f2232d4c32c4e965d3945c7829edd70b1ac397e4
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2021-11-20 00:04:38 +0900

    exercise : work with dates and times

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	n := time.Now();
+	fmt.Println("Current Time is ", n);
 
-	fmt.Println("Dates and times")
+	france, err := time.LoadLocation("Europe/Paris");
+	if err != nil {
+		panic(err)
+	}
 
+
+	t := time.Date(1986, time.July, 17, 11,0,0,0, france);
+	fmt.Println("I was born on ", t);
+	fmt.Println(t.Format(time.ANSIC))
+
+	parsedTime, err := time.Parse(time.ANSIC, "Thu Jul 17 11:00:00 1986")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("The type of parsedTime is %T \n", parsedTime);
 }
