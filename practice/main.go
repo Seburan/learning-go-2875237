e8ace33061967a89e0a49323d3da737fef5e49be
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2021-11-21 22:51:42 +0900

    exercise arrays

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,17 @@ import (
 
 func main() {
 	fmt.Println("Arrays")
+
+	var colors [3]string;
+	colors[0] = "Blue";
+	colors[1] = "White";
+	colors[2] = "Red";
+	fmt.Println(colors);
+	fmt.Println(colors[0]);
+
+	var numbers = [5]int{1,2,3,4,5}
+	fmt.Println(numbers)
+
+	fmt.Println("Numbers of colors : ", len(colors));
+	fmt.Println("Numbers of numbers : ", len(numbers));
 }
