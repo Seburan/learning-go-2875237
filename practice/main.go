10147cd00fe58d8539b0a585442b9d4a1fd992b5
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2021-11-21 22:45:48 +0900

    exercise pointers

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,17 @@ import (
 
 func main() {
 	fmt.Println("Pointers")
+
+	anInt := 42;
+	var p = &anInt;
+	fmt.Println("Value of p = ", *p);
+
+	value1 := 42.13
+	pointer1 := &value1;
+	fmt.Println("Value of value1 = ", *pointer1)
+
+	*pointer1 = *pointer1 / 31;
+	fmt.Println("Value of *pointer1 = ", *pointer1);
+	fmt.Println("Value of value1 = ", value1)
+
 }
