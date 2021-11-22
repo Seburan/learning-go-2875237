25a5b4fa8f395383e8af42f1c53a1b548c2efbc0
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2021-11-22 23:08:44 +0900

    exercise : maps

--- practice/main.go
+++ practice/main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	fmt.Println("Maps")
+
+	states := make(map[string]string);
+	fmt.Println(states);
+	states["WA"] = "Washington";
+	states["OR"] = "Oregen";
+	states["CA"] = "California";
+	fmt.Println(states);
+
+	california := states["CA"];
+	fmt.Println(california);
+
+	delete(states, "OR")
+	states["NY"] = "New York"
+	fmt.Println(states);
+
+	for k, v := range states {
+		fmt.Printf("%v: %v\n", k, v);
+	}
+
+	keys := make([]string, len(states));
+	i := 0;
+	for k := range states {
+			keys[i] = k;
+			i++;
+	}
+	fmt.Println(keys);
+	sort.Strings(keys);
+	fmt.Println(keys);
+
+	for _, k := range keys {
+		fmt.Printf("%v: %v\n", k, states[k]);
+	}
+
 }
