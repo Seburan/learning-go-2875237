2b772e091f2a3563195b200379131668839f10cd
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2021-11-23 22:22:50 +0900

    exercise : structs

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,22 @@ import (
 
 func main() {
 	fmt.Println("Structs")
+
+	poodle := Dog{
+		Breed:  "Poodle",
+		Weight: 10,
+	}
+
+	fmt.Println(poodle);
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed : %v\nWeight : %v\n", poodle.Breed, poodle.Weight);
+	poodle.Weight = 9;
+	fmt.Printf("Breed : %v\nWeight : %v\n", poodle.Breed, poodle.Weight);
+
+}
+
+// dog is a struct
+type Dog struct {
+	Breed string;
+	Weight int;
 }
