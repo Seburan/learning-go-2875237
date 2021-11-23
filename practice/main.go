f7e937c89cb1f8e7c1d15f114b6f077ece10e3c2
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2021-11-23 22:57:11 +0900

    functions

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,30 @@ import (
 
 func main() {
 	fmt.Println("Functions")
+	doSomethings();
+
+	sum := addValues(5, 8);
+	fmt.Println("The sum is ", sum)
+
+	multiSum, multiCount := addAllValues(4,7,9,10);
+	fmt.Println("The sum is ", multiSum)
+	fmt.Println("Count of items", multiCount)
+}
+
+func doSomethings() {
+	fmt.Println("Doing Something")
+}
+
+func addValues(value1, value2 int) int {
+	return value1 + value2;
+}
+
+func addAllValues(values ...int) (int, int) {
+	total := 0;
+	for _, v := range values {
+		total += v;
+	}
+
+	return total, len(values);
+
 }
