1f46ff7ae4729f8a5e16eeea32d648fa9c79586d
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2021-11-25 09:42:56 +0900

    exercise : advanced calculator

--- calculator/main.go
+++ calculator/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -12,24 +11,68 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Value 1: ")
-	input1, _ := reader.ReadString('\n')
-	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 1 must be a number")
+	float1 := inputValue("Value 1")
+	float2 := inputValue("Value 2")
+	var result float64;
+
+	fmt.Print("Select an operation (+ - * /) : ")
+	inputString, _ := reader.ReadString('\n')
+	operationString := strings.TrimSpace(inputString)
+
+	switch operationString {
+	case "+":
+		result = calcAddition(float1, float2)
+		fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, result)
+	case "-":
+		result = calcSubstraction(float1, float2)
+		fmt.Printf("The substraction of %v and %v is %v\n\n", float1, float2, result)
+	case "*":
+		result = calcMultiplication(float1, float2)
+		fmt.Printf("The multiplication of %v by %v is %v\n\n", float1, float2, result)
+	case "/":
+		result = calcDivision(float1, float2)
+		fmt.Printf("The division of %v by %v is %v\n\n", float1, float2, result)
+	default:
+		panic("Incorrect Operation")
 	}
 
-	fmt.Print("Value 2: ")
-	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+	// result := calcAddition(float1, float2)
+	// sum = math.Round(sum*100) / 100
+	// fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, result)
+
+}
+
+// obtain user input of a number
+func inputValue(valueName string) float64 {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("%v : ", valueName)
+	inputString, _ := reader.ReadString('\n')
+	inputFloat, err := strconv.ParseFloat(strings.TrimSpace(inputString), 64)
 	if err != nil {
 		fmt.Println(err)
-		panic("Value 2 must be a number")
+		panic(fmt.Sprintf("%v must be a number", valueName))
 	}
 
-	sum := float1 + float2
-	sum = math.Round(sum*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+	return inputFloat
+}
 
+// sum of v1 and v2
+func calcAddition(v1, v2 float64) float64 {
+	return v1 + v2
+}
+
+// substraction of v1 and v2
+func calcSubstraction(v1, v2 float64) float64 {
+	return v1 - v2
+}
+
+// Multiplication of v1 and v2
+func calcMultiplication(v1, v2 float64) float64 {
+	return v1 * v2
+}
+
+// substraction of v1 and v2
+func calcDivision(v1, v2 float64) float64 {
+	return v1 / v2
 }
