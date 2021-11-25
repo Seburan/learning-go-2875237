216c5a554f0ed1997e4f16858e71ef771f653838
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2021-11-25 22:54:22 +0900

    json formatted text

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
@@ -11,22 +14,57 @@ const url = "http://services.explorecalifornia.org/json/tours.php"
 func main() {
 	fmt.Println("Network requests")
 
-	response,err := http.Get(url);
+	response, err := http.Get(url)
 	if err != nil {
-		panic(err);
+		panic(err)
 	}
 
-	fmt.Printf("Response type: %T\n", response);
+	fmt.Printf("Response type: %T\n", response)
 
-	defer response.Body.Close();
+	defer response.Body.Close()
 
-	bytes, err := io.ReadAll(response.Body);
+	bytes, err := io.ReadAll(response.Body)
 
 	if err != nil {
-		panic(err);
+		panic(err)
 	}
 
-	content := string(bytes);
-	fmt.Print(content);
+	content := string(bytes)
+	// fmt.Print(content)
 
+	tours := toursFromJson(content);
+	for _, tour := range tours {
+		price, err := strconv.ParseFloat(tour.Price, 64)
+		if err != nil {
+			panic(err);
+		}
+		fmt.Printf("%v - %v\n", tour.Name, price )
+	}
+
+}
+
+func toursFromJson(content string) []Tour {
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	if err != nil {
+		panic(err)
+	}
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		if err != nil {
+			panic(err)
+		}
+		tours = append(tours, tour);
+	}
+
+	return tours;
+}
+
+type Tour struct {
+	Name string
+	Price string
 }
