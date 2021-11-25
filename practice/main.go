2c4316c5bbb5e4e8e0525f4bb776f884047d587f
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2021-11-25 22:36:35 +0900

    read a text file from the web

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	fmt.Println("Network requests")
+
+	response,err := http.Get(url);
+	if err != nil {
+		panic(err);
+	}
+
+	fmt.Printf("Response type: %T\n", response);
+
+	defer response.Body.Close();
+
+	bytes, err := io.ReadAll(response.Body);
+
+	if err != nil {
+		panic(err);
+	}
+
+	content := string(bytes);
+	fmt.Print(content);
+
 }
